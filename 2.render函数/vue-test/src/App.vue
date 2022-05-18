692f9348626f9601d8f636d1a383877b511a22d5
耿惠莉 <12345678>
2022-05-18 14:03:25 +0800

    feat: 添加render函数

---
+++ 2.render函数/vue-test/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <span>render函数</span>
+    <RenderTest tag="ul" />
+  </div>
+</template>
+
+<script>
+import RenderTest from './components/RenderTest.vue'
+
+export default {
+  name: 'App',
+  components: {
+    RenderTest
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ 2.render函数/vue-test/src/components/RenderTest.vue
@@ -0,0 +1,35 @@
+<script>
+export default {
+  name: "HelloWorld",
+  props: {
+    tag: String,
+  },
+  data() {
+    return {
+      peopleList: ["张三1", "张三2", "张三3"],
+    };
+  },
+  render(createElement) {
+    return createElement(
+      this.tag,
+      {},
+      this.peopleList.map((name) => createElement("li", {attrs:{class:"custom-text"}}, name))
+    );
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+.custom-text{
+  color:cadetblue;
+}
+</style>
